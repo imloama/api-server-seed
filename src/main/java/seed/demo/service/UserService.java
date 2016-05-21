eb4d1a7962d98ff6137660e4cca0e9cd8582c982
mazhaoyong <mazhaoyong@gmail.com>
2016-05-21 15:52:34 +0800

    添加dubbo依赖

--- src/main/java/seed/demo/service/UserService.java
+++ src/main/java/seed/demo/service/UserService.java
@@ -1,13 +1,21 @@
 package seed.demo.service;
 
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.dubbo.config.annotation.Service;
 
 import seed.core.service.BaseService;
 import seed.demo.mapper.UserMapper;
 import seed.demo.models.User;
 
-@Service
+
+/**
+ * 注解采用dubbo的@service，为了采用dubbo提供的服务
+ * 具体使用可以见本人的项目：https://github.com/mazhaoyong/dubbox-zookeeper-demo
+ * 
+ * @author mazhaoyong
+ */
+@Service(protocol = { "dubbo" })
 @Transactional
 public class UserService extends BaseService<User>{
 	
