3e86e9530bbcd10921a9c77431ace76adae070f2
马兆永 <mazhaoyong@gmail.com>
2018-12-28 22:00:17 +0800

    init v2

---
+++ src/main/java/com/github/imloama/apiseed/ApiSeedApplication.java
@@ -0,0 +1,14 @@
+package com.github.imloama.apiseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSeedApplication.class, args);
+	}
+
+}
+
